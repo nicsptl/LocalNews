10eed9a6f81622c153c22e27057a9ab1103973ad
Nikul Patel <Nikul Patel>
2017-05-22 12:34:33 -0500

    added template for comment section

--- app.js
+++ app.js
@@ -71,6 +71,19 @@ var app = angular.module('flapperNews', ['ui.router']);
         'posts',
         function($scope, $stateParams, posts){
             $scope.post = posts.posts[$stateParams.id];
+
+            $scope.addComment = function(){
+                if($scope.body === ''){ return;}
+                $scope.post.comments.push(
+                    {
+                        body: $scope.body,
+                        author:'user',
+                        upvotes : 0,
+
+                    });
+                $scope.body ='';
+            }
+
     }]);
 
 
